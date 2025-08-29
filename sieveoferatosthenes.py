95e76b44a8bf5362809e03cb931825ba76cd6191
Chris Webb <50113303+CodeDrome@users.noreply.github.com>
2025-08-29 16:31:24 +0100

    Add files via upload

--- sieveoferatosthenes.py
+++ sieveoferatosthenes.py
@@ -1,6 +1,10 @@
+from typing import List
+
+
 PRIME_COL = "\x1B[32m"
 COMPOSITE_COL = "\x1B[31m"
 RESET_COL = "\x1B[0m"
+
 
 def main():
 
@@ -24,6 +28,7 @@ def main():
     print("Running Sieve of Eratosthenes...")
 
     while(p != -1):
+
         mark_multiples(sieve, max, p)
         p = find_next_prime(sieve, max, p)
 
@@ -32,7 +37,8 @@ def main():
     print(COMPOSITE_COL + "Composite numbers" + RESET_COL)
     print_sieve(sieve, 10)
 
-def print_sieve(sieve, columncount):
+
+def print_sieve(sieve: List, columncount: int):
 
     """
     Prints the sieve in columns of specified size.
@@ -43,14 +49,18 @@ def print_sieve(sieve, columncount):
     for i in range(1, len(sieve)):
 
         if sieve[i] == True:
-            print(PRIME_COL + "%4d" % i + RESET_COL, end="")
-        else:
-            print(COMPOSITE_COL + "%4d" % i + RESET_COL, end="")
 
-        if i % 10 == 0:
+            print(f"{PRIME_COL}{i:4d}{RESET_COL}", end="")
+
+        else:
+
+            print(f"{COMPOSITE_COL}{i:4d}{RESET_COL}", end="")
+
+        if i % columncount == 0:
             print("")
 
-def mark_multiples(sieve, max, p):
+
+def mark_multiples(sieve: List, max: int, p: int):
 
     """
     Given a prime, mark all its multiples as False (ie non-prime or composite)
@@ -61,12 +71,15 @@ def mark_multiples(sieve, max, p):
     i = p * multiplier
 
     if i <= max:
+
         while(i <= max):
+
             sieve[i] = False
             multiplier+=1
             i = p * multiplier
 
-def find_next_prime(sieve, max, current):
+
+def find_next_prime(sieve: List, max: int, current: int):
 
     """
     Iterates remaining part of sieve to find next prime.
@@ -75,11 +88,14 @@ def find_next_prime(sieve, max, current):
     """
 
     for i in range(current + 1, max + 1):
+
         if sieve[i] == True:
+
             return i
 
-    return -1;
+    return -1
 
-#-----------------------------------------------------
 
-main()
+if __name__ == "__main__":
+
+    main()
